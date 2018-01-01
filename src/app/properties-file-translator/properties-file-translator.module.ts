6bed161207b90f98154bb1dde2c4c75ba6d57e5c
nhduc <nhduc.seuit@gmail.com>
2018-01-01 19:34:21 +0700

    Fix path redirection not working

--- src/app/properties-file-translator/properties-file-translator.module.ts
+++ src/app/properties-file-translator/properties-file-translator.module.ts
@@ -3,12 +3,14 @@ import { SharedModule } from './../shared/shared.module';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { PropertiesFileTranslatorComponent } from './properties-file-translator.component';
+import { PropertiesFileTranslatorRoutingModule } from 'app/properties-file-translator/properties-file-translator-routing.module';
 import { LocalStorageService } from '../shared/services/local-storage.service';
 
 @NgModule({
   imports: [
     CommonModule,
     SharedModule,
+    PropertiesFileTranslatorRoutingModule
   ],
   declarations: [
     PropertiesFileTranslatorComponent,
